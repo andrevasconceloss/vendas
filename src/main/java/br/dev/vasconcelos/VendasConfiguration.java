f65b385a7de12812c608b23676f241c51605fc3f
André Vasconcelos <andre.ovasconceloss@gmail.com>
2022-01-27 20:36:34 -0300

    Alterações pontuais.

--- src/main/java/br/dev/vasconcelos/VendasConfiguration.java
+++ src/main/java/br/dev/vasconcelos/VendasConfiguration.java
@@ -6,8 +6,5 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class VendasConfiguration {
 
-    @Bean(name = "applicationName")
-    public String apllicationName(){
-        return "Sistema de vendas";
-    }
+
 }
