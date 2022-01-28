f70e15290ec696e95f40d96c377302121b36ac2e
André Vasconcelos <andre.ovasconceloss@gmail.com>
2022-01-27 22:01:19 -0300

    Alterações para criar configurações para cada ambiente.

--- src/main/java/br/dev/vasconcelos/VendasConfiguration.java
+++ src/main/java/br/dev/vasconcelos/VendasConfiguration.java
@@ -1,10 +1,16 @@
 package br.dev.vasconcelos;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class VendasConfiguration {
 
-
+    @Bean
+    public CommandLineRunner executar() {
+        return args -> {
+            System.out.println("RODANDO DESENVOLVIMENTO");
+        };
+    }
 }
