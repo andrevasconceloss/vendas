b0050a65b55840f0a2387482c0613021223713a9
André Vasconcelos <andre.ovasconceloss@gmail.com>
2022-01-28 14:42:05 -0300

    Alterações pontuais.

--- src/main/java/br/dev/vasconcelos/VendasConfiguration.java
+++ src/main/java/br/dev/vasconcelos/VendasConfiguration.java
@@ -1,5 +1,6 @@
 package br.dev.vasconcelos;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,10 +8,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class VendasConfiguration {
 
+    @Value("${application.environment}")
+    private String applicationEnvironment;
+
     @Bean
     public CommandLineRunner executar() {
         return args -> {
-            System.out.println("RODANDO DESENVOLVIMENTO");
+            System.out.println("API em funcionamente: " + applicationEnvironment);
         };
     }
 }
