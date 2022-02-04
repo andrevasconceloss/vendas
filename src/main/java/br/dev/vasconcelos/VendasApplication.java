4d2bd9205f87a1e3d375d32a97bb6740a2ce6c36
André Vasconcelos <andre.ovasconceloss@gmail.com>
2022-02-04 11:56:17 -0300

    Alteração somente para teste dos métodos de Clientes

--- src/main/java/br/dev/vasconcelos/VendasApplication.java
+++ src/main/java/br/dev/vasconcelos/VendasApplication.java
@@ -1,14 +1,28 @@
 package br.dev.vasconcelos;
 
+import br.dev.vasconcelos.model.Cliente;
+import br.dev.vasconcelos.repository.ClientesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired ClientesRepository clientes) {
+        return args -> {
+            Cliente cliente = new Cliente();
+            cliente.setNome("André");
+            clientes.salvar( cliente );
+        };
+    }
 
     @Value("${application.name}")
     private String applicationName;
