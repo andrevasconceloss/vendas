7404c79fb59adc38df10c86c9ac89c3a17cab67a
André Vasconcelos <andre.ovasconceloss@gmail.com>
2022-02-08 10:28:50 -0300

    Criação de novos método para deletar e altera o cliente.

--- src/main/java/br/dev/vasconcelos/repository/ClientesRepository.java
+++ src/main/java/br/dev/vasconcelos/repository/ClientesRepository.java
@@ -13,24 +13,54 @@ import java.util.List;
 @Repository
 public class ClientesRepository {
 
-    private static String INSERT = "INSERT INTO CLIENTE (NOME) VALUES (?)";
-    private static String SELECT_ALL = "SELECT * FROM CLIENTE";
+    private static String INSERT = " INSERT INTO CLIENTE (NOME) VALUES (?) ";
+    private static String UPDATE = " UPDATE CLIENTE SET NOME= ? WHERE ID= ? ";
+    private static String DELETE = " DELETE FROM CLIENTE WHERE ID= ? ";
+    private static String SELECT = " SELECT * FROM CLIENTE ";
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public Cliente salvar(Cliente cliente) {
+    public Cliente save(Cliente cliente) {
         // Salva cliente na base
         jdbcTemplate.update( INSERT, new Object[]{cliente.getNome()} );
         return cliente;
     }
 
-    public List<Cliente> all() {
-        return jdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>() {
+    public Cliente update(Cliente cliente) {
+        jdbcTemplate.update(UPDATE, new Object[]{cliente.getNome(), cliente.getId()});
+        return cliente;
+    }
+
+    public void delete(Cliente cliente) {
+        jdbcTemplate.update(UPDATE, new Object[]{cliente.getId()});
+    }
+
+    public void delete(Integer id) {
+        jdbcTemplate.update(UPDATE, id);
+    }
+
+    public List<Cliente> searchForName(String name) {
+        return jdbcTemplate.query(
+                SELECT.concat(" WHERE UPPER(NAME) LIKE ? "),
+                new Object[]{"%" + name.toUpperCase() + "%"},
+                getClienteMapper());
+    }
+
+    public List<Cliente> searchAll() {
+        return jdbcTemplate.query(SELECT, getClienteMapper());
+    }
+
+    private RowMapper<Cliente> getClienteMapper() {
+        return new RowMapper<Cliente>() {
             @Override
             public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
-                return new Cliente(resultSet.getString("nome"));
+                Cliente cliente = new Cliente();
+                cliente.setNome(resultSet.getString("nome"));
+                cliente.setId(resultSet.getInt("id"));
+                return cliente;
             }
-        });
+        };
     }
+
 }
