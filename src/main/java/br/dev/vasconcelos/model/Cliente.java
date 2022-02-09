3352d7da442d3484b18a0542814d31dba0f7e699
André Vasconcelos <andre.ovasconceloss@gmail.com>
2022-02-09 15:15:25 -0300

    Mapeamento da classe para utilizar JPA

--- src/main/java/br/dev/vasconcelos/model/Cliente.java
+++ src/main/java/br/dev/vasconcelos/model/Cliente.java
@@ -1,14 +1,23 @@
 package br.dev.vasconcelos.model;
 
-import lombok.Data;
+import javax.persistence.*;
 
-public @Data class Cliente {
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "nome", length = 100, nullable = false)
     private String nome;
 
     public Cliente() {
 
     }
+
     public Cliente(String name) {
         this.nome = name;
     }
